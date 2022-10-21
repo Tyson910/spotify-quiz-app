b8f5637226e26de811d72d56bb6ce926045709ba
Tyson <TysonS.417@gmail.com>
2022-10-21 16:46:54 -0400

    add tailwind to project

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,svelte,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
