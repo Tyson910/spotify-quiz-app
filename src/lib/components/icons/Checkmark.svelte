b7077de183271d0fc3a47e2eb4aa882f7db1c199
Tyson <TysonS.417@gmail.com>
2023-01-02 19:04:02 -0500

    create Checkmark component

---
+++ src/lib/components/icons/Checkmark.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import classnames from 'classnames';
+	export let svgClasses = '';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	class={classnames('w-6 h-6', svgClasses)}
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+</svg>
