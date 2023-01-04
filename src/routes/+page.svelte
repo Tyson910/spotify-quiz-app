cdfb1ddb7550bfd7c5a4581cdfe19b7258f8a057
Tyson <TysonS.417@gmail.com>
2023-01-04 16:11:17 -0500

    create home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { ActionData } from './$types';
+	import Features from '@components/homepage/Features.svelte';
+	import Hero from '@components/homepage/Hero.svelte';
+	import ArtistListItem from '@components/homepage/ArtistListItem.svelte';
+	export let form: ActionData;
+</script>
+
+<Hero isFormMissingArtist={form?.missing} />
+
+{#if form?.artists}
+	{#each form.artists as artist (artist.id)}
+		<div class="mx-auto max-w-3xl">
+			<ArtistListItem {artist} />
+		</div>
+	{/each}
+{/if}
+<Features />
