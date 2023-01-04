87b4e9cf288424b5c7fc7f67fdac91e9767f2d88
Tyson <TysonS.417@gmail.com>
2023-01-04 16:10:20 -0500

    create formAction for artist search

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,23 @@
+import { findArtist } from "$lib/server/Spotify";
+import { fail } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+  default: async (event) => {
+    const data = await event.request.formData();
+    const userSearchInput = data.get("artist-search");
+    if (
+      !userSearchInput || typeof userSearchInput != "string" ||
+      typeof userSearchInput != "number"
+    ) {
+      return fail(400, { userSearchInput, missing: true });
+    }
+
+    try {
+      const artists = await findArtist(userSearchInput.toString());
+      return { missing: false, artists };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
