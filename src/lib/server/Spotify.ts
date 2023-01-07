98e4ae293f4e8bcdc7dafccb91194dfcfd541b3a
Tyson <TysonS.417@gmail.com>
2023-01-07 14:21:29 -0500

    get tracks & artist

--- src/lib/server/Spotify.ts
+++ src/lib/server/Spotify.ts
@@ -58,8 +58,8 @@ async function findArtist(
 ): Promise<SpotifyApi.ArtistObjectFull[]> {
   try {
     // %20tag:hipster add
-    const response: SpotifyApi.ArtistSearchResponse = await fetchFromSpotify(
-      `/search?type=artist&limit=5&q=${encodeURIComponent(artistQuery)}`,
+    const response: SpotifyApi.ArtistSearchResponse & SpotifyApi.TrackSearchResponse = await fetchFromSpotify(
+      `/search?type=artist,tracks&limit=5&q=${encodeURIComponent(artistQuery)}`,
     );
     if (!response?.artists?.items || response.artists.items.length < 1) {
       throw new Error("Artist couldnt be found");
