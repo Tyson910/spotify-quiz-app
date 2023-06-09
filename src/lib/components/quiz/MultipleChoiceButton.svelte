aaad5a924a996949178d1e8b14490be2916da36f
Tyson <TysonS.417@gmail.com>
2023-06-09 17:59:44 -0400

    create Mutliplechoicebutton component

---
+++ src/lib/components/quiz/MultipleChoiceButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import classnames from 'classnames';
+	export let trackName = '';
+	export let classes = '';
+</script>
+
+<button
+	on:click
+	class={classnames(
+		'py-2 px-5 text-center',
+		'flex justify-center items-center max-w-sm w-full mx-auto',
+		'rounded-full border border-current',
+		'hover:transition-colors hover:duration-700',
+		'focus:outline-none focus:ring-2 focus:ring-current focus:ring-offset-2',
+		classes
+	)}
+>
+	{trackName}
+	<slot />
+</button>
