96c669af0769a7a66bc3dd60518b9b9c9db458d8
Tyson <TysonS.417@gmail.com>
2023-06-11 10:38:08 -0400

    remove unused Spotify functions and export SPOTIFY_API_ENDPOINT constant

--- src/lib/server/Spotify.ts
+++ src/lib/server/Spotify.ts
@@ -1,118 +1,72 @@
 import { CLIENT_ID, CLIENT_SECRET } from "$env/static/private";
+const SPOTIFY_API_ENDPOINT = "https://api.spotify.com/v1";
 
-async function getAccessToken(): Promise<string | null> {
+interface SpotifyAccessToken {
+  /** An Access Token that can be provided in subsequent calls, for example to Spotify Web API services. */
+  access_token: string;
+  /** How the Access Token may be used: always "Bearer". */
+  token_type: string;
+  /** A space-separated list of scopes which have been granted for this access_token. */
+  scope: string;
+  /** The time period (in seconds) for which the Access Token is valid. */
+  expires_in: number;
+  /**
+   * A token that can be sent to the Spotify Accounts service in place of an authorization code.
+   * (When the access code expires, send a POST request to the Accounts service /api/token endpoint,
+   * but use this code in place of an authorization code. A new Access Token will be returned.
+   * A new refresh token might be returned too.)
+   */
+  refresh_token: string;
+}
+
+async function requestAccessToken(authCode: string) {
   const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-  const fetchOptions = {
-    method: "POST",
-    body: "grant_type=client_credentials",
-    headers: {
-      "Authorization": "Basic " +
-        (Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64")),
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  };
 
-  interface ApiTokenResponse {
-    access_token?: string;
-    token_type?: "bearer";
-    expires_in?: number;
-  }
   try {
-    const response = await fetch(TOKEN_ENDPOINT, fetchOptions);
-
+    const response = await fetch(TOKEN_ENDPOINT, {
+      method: "POST",
+      body: new URLSearchParams({
+        code: authCode,
+        redirect_uri: "http://localhost:5173/confirmed",
+        grant_type: "authorization_code",
+      }),
+      headers: {
+        "Authorization": "Basic " +
+          (Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64")),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
     if (response.status != 200) throw new Error("Unable to fetch data");
-    const { access_token }: ApiTokenResponse = await response.json();
-    return access_token || null;
+    const spotifyAccessToken: SpotifyAccessToken = await response.json();
+    return spotifyAccessToken;
   } catch (error) {
     console.log(error);
     return null;
   }
 }
 
-async function fetchFromSpotify<Type>(query: string): Promise<Type> {
-  if (!query.startsWith("/")) {
-    throw new Error(
-      "Invalid query! Please make sure query string begins with a '/' ",
-    );
-  }
+async function requestRefreshToken(refresh_token: string) {
+  const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
-  const SPOTIFY_API_ENDPOINT = "https://api.spotify.com/v1";
-  const accessToken = await getAccessToken();
-
-  if (!accessToken) throw new Error("Couldn't generate valid access token!");
-
-  const response = await fetch(SPOTIFY_API_ENDPOINT + query, {
-    headers: {
-      "Authorization": "Bearer " + accessToken,
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (response.status != 200) throw new Error("ERROR! Probably Auth issue");
-
-  return response.json();
-}
-
-async function findArtist(
-  artistQuery: string,
-): Promise<SpotifyApi.ArtistObjectFull[]> {
   try {
-    // %20tag:hipster add
-    const response: SpotifyApi.ArtistSearchResponse = await fetchFromSpotify(
-      `/search?type=artist,track&limit=5&q=${encodeURIComponent(artistQuery)}`,
-    );
-    if (!response?.artists?.items || response.artists.items.length < 1) {
-      throw new Error("Artist couldnt be found");
-    }
-    return response.artists.items.sort((a, b) => b.popularity - a.popularity);
-  } catch (err) {
-    console.log(err);
-    return [];
-  }
-}
-
-async function getArtistTracks(
-  artistName: string,
-): Promise<SpotifyApi.TrackObjectFull[]> {
-  try {
-    // %20tag:hipster add
-    const response: SpotifyApi.TrackSearchResponse = await fetchFromSpotify(
-      `/search?type=track&limit=50&q=${encodeURIComponent(artistName)}`,
-    );
-    if (!response?.tracks?.items || response.tracks.items.length < 1) {
-      throw new Error("Artist couldnt be found");
-    }
-    response.tracks.items = removeDuplicateTracks(response.tracks.items);
-    // fetching 50 tracks and removing duplicates, game should be good with at least 20-25 songs,
-    // v suprising if an artist doesn't have at least 20 songs
-    if (response.tracks.items.length < 20) {
-      throw new Error("Not enough songs found to play a game");
-    }
-    return response.tracks.items;
-    // return response.tracks.items.sort((a, b) => b.popularity - a.popularity);
-  } catch (err) {
-    console.log(err);
-    return [];
-  }
-}
-
-async function getTopTracksFromArtist(
-  artistID: SpotifyApi.ArtistObjectFull["id"],
-): Promise<SpotifyApi.ArtistsTopTracksResponse["tracks"]> {
-  try {
-    // %20tag:hipster add
-    const { tracks }: SpotifyApi.ArtistsTopTracksResponse =
-      await fetchFromSpotify(
-        `/artists/${artistID}/top-tracks?market=US`,
-      );
-    if (!tracks || tracks?.length < 1) {
-      throw new Error("Artist top tracks couldnt be found");
-    }
-    // TODO: filter out any tracks that dont include a preview id
-    return tracks;
-  } catch (err) {
-    console.log(err);
-    return [];
+    const response = await fetch(TOKEN_ENDPOINT, {
+      method: "POST",
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token
+      }),
+      headers: {
+        "Authorization": "Basic " +
+          (Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64")),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    if (response.status != 200) throw new Error("Unable to fetch data");
+    const spotifyAccessToken: SpotifyAccessToken = await response.json();
+    return spotifyAccessToken;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 }
 
@@ -126,9 +80,4 @@ function removeDuplicateTracks(
   return removeDupeTracks;
 }
 
-export {
-  fetchFromSpotify,
-  findArtist,
-  getArtistTracks,
-  getTopTracksFromArtist,
-};
+export { requestAccessToken, requestRefreshToken, SPOTIFY_API_ENDPOINT };
