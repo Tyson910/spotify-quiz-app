e35262df3e2bfe072008f955eda84abeb9332ba7
Tyson <TysonS.417@gmail.com>
2023-06-24 20:00:58 -0400

    add iconify support

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
+import type * as Icons from "unplugin-icons/types/svelte";
+export default Icons;
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Error {}
-	// interface Locals {}
-	// interface PageData {}
-	// interface Platform {}
+  // interface Error {}
+  // interface Locals {}
+  // interface PageData {}
+  // interface Platform {}
 }
+
+export {};
