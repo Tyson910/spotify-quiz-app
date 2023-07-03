244cabd9c0d63810e42075eaed53ca8a95844a7b
Tyson <TysonS.417@gmail.com>
2023-07-03 15:55:54 -0400

    add user to Locals interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,13 @@
-import type * as Icons from "unplugin-icons/types/svelte";
-export default Icons;
+/// <reference types="unplugin-icons/types/svelte" />
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
   // interface Error {}
-  // interface Locals {}
+  interface Locals {
+    user: SpotifyApi.CurrentUsersProfileResponse | null;
+  }
   // interface PageData {}
   // interface Platform {}
 }
 
-export {};
